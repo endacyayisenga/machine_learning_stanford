c3503e7c780d1df9489e6e653c47fba2f59badfb
philbort <phduan@gmail.com>
2016-09-13 21:40:26 -0700

    clean up

--- exercise6/ex6.m
+++ exercise6/ex6.m
@@ -17,7 +17,7 @@
 %
 
 %% Initialization
-clear ; close all; clc
+clear; close all; clc
 
 %% =============== Part 1: Loading and Visualizing Data ================
 %  We start the exercise by first loading and visualizing the dataset. 
